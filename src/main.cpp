279cae29667c5e8e2786bad1638665755127e795
Alex Landherr <alex.landherr@gmail.com>
2023-09-11 10:33:55 +0000

    Init commit.

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/*
+The number 13195 consists of the prime factors 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143?
+*/
+
+int main() {
+
+    return 0;
+}
